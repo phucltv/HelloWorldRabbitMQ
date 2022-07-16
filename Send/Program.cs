0546d00314564d9e6e7e5e4763555188f0cc6633
phuc <phuc@phuc>
2022-07-16 10:00:43 +0700

    Add project files.

---
+++ Send/Program.cs
@@ -0,0 +1,27 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+Console.WriteLine("Producer");
+
+var factory = new ConnectionFactory() { HostName = "localhost" };
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+var queueName = "hello";
+
+channel.QueueDeclare(queueName,
+    durable: false,
+    exclusive: false, 
+    autoDelete: false, 
+    arguments: null);
+
+var message = "Hello World";
+var body = Encoding.UTF8.GetBytes(message);
+
+channel.BasicPublish(exchange: "", 
+    routingKey: queueName, 
+    basicProperties: null, 
+    body: body);
+
+Console.WriteLine("[x] Send message: {0}", message);
+
+Console.ReadLine();
